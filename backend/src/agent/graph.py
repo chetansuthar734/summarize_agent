95db2920c78da83af665bc00f031cca89f72a0a8
CHETAN KUMAR <99160601+chetansuthar734@users.noreply.github.com>
2025-08-21 07:31:29 +0530

    Update graph.py

--- backend/src/agent/graph.py
+++ backend/src/agent/graph.py
@@ -3,12 +3,13 @@ from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import StateGraph, END,START ,MessagesState
 from langchain_core.messages import SystemMessage, HumanMessage,AIMessage,ToolMessage
 
+from langchain.prompts import ChatPromptTemplate
 
 from typing import TypedDict, List,Annotated
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 # model = ChatGoogleGenerativeAI(model='gemini-2.0-flash',api_key="AIzaSyDK1CNcAh***********k2U51Rug",disable_streaming=True)
-stream_model = ChatGoogleGenerativeAI(model='gemini-2.0-flash',api_key="AIzaSyDK1CN*************XLu7J7Qk2U51Rug")
+stream_model = ChatGoogleGenerativeAI(model='gemini-1.5-flash',api_key="AIzaSyDK1CNcAhSrM4qy3UVIXLu7J7Qk2U51Rug")
 
 
 
@@ -18,14 +19,16 @@ import time
 
 def summarize_node(state:MessagesState):
     writer =get_stream_writer()
-    topic = state['messages'][-1].content
+    message = state['messages'][-1].content
     writer('starting summarazing topic.......')
-    time.sleep(2)
-    content = "\n".join(msg.content for msg in state["messages"][-1])
+    # print(state['messages'][-1])
     # summary  = f"""this is  summamry on given by llm on  topic}"""
-    prompt = ChatPromptTemplate.from_messages([SystemMessage(content="your are helpful assistant , summarize the content by user "),HumanMessage(content=f"{content}")])
+    context = state["messages"][-1].content
+    # print(context)
+
+    prompt = ChatPromptTemplate.from_messages([('system',"your are helpful assistant , summarize the content given by user  in ~50 word to ~100 word"),("human","{context}")]).format_messages(context=context)
     summary = stream_model.invoke(prompt)
-    return {"messages": [AIMessage(content=summary)]}
+    return {"messages": [summary]}
 
 # Setup the graph
 builder = StateGraph(MessagesState)
@@ -34,3 +37,4 @@ builder.add_node("summarize", summarize_node)
 builder.add_edge(START,"summarize")
 
 graph  = builder.compile()
+
